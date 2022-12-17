43411740c948d5883d03d526102434cb7530b925
Niord19 <630153499@qq.com>
2022-12-17 18:55:36 +0800

    Site updated: 2022-12-17 18:55:36

--- js/wowjs/wow_init.js
+++ js/wowjs/wow_init.js
@@ -1,4 +1,4 @@
-wow = new WOW({
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>wow = new WOW({
   boxClass: 'wow',
   // 当用户滚动时显示隐藏框的类名称
   animateClass: 'animate__animated',
